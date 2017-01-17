c15b07367098904db78ae60ddc034298f5543c80
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-17 11:06:04 -0500

    woo front end! my fav!

--- app/assets/javascripts/tags/index_view.js
+++ app/assets/javascripts/tags/index_view.js
@@ -7,9 +7,37 @@ APP.view = (function($){
     $pic = $("#waldo-pic")
     $pic.on("mouseover", callbacks.pictureEnter)
     $pic.on("mouseout", callbacks.pictureExit)
-  },
+  };
 
-  var showTags = function(tags){
-    
-  }
+  var createTags = function(tags){
+    tags.forEach(function(tag){
+      $()
+
+    })
+  };
 })($)
+
+var renderBoard = function renderBoard(board) {
+  var size = CELL_SIDE,
+  square;
+
+  gameWrapper.innerHTML = "";
+
+  for(coord in board) {
+  square = document.createElement('DIV');
+  square.classList.add('square');
+   square.id = coord;
+
+  if (board[coord].value) {
+  square.classList.add(board[coord].value)
+  }
+
+  square.style.height = size;
+  square.style.width = size;
+  square.style.top = board[coord].y*size + "px";
+  square.style.left = board[coord].x*size + "px";
+  square.style.zIndex = '0';
+
+  gameWrapper.appendChild(square);
+  }
+  }
